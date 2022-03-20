e148317350a8de803426b3c29e6f66e69887f42b
Kancabricot <92451452+Kancabricot@users.noreply.github.com>
2022-03-20 17:41:54 +0100

    Rajout d'une map

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -1,131 +1,75 @@
 class Tableau1 extends Phaser.Scene {
 
+
     preload() {
-        // le preload des images
-        this.load.image('Idle','assets/Perso/Idle.png');
-        this.load.image('platform','assets/square.png');
+        // Je preload les images autres que Tiled
         this.load.image('circle','assets/circle.png');
         this.load.image('circleG','assets/circleG.png');
 
-        for (let i = 0; i < 5; i++) {
-            this.load.image('w'+i,'assets/Perso/w'+i+'.png');
-        }
+        this.load.image('platform','assets/square.png');
 
+        // chargement tilemap
+        this.load.image("tilemap", "assets/tiles_packed.png");
 
-
-        this.load.image('background', 'assets/images/background.png');
-        this.load.image('spike', 'assets/images/spike.png');
-
-        this.load.atlas('player', 'assets/images/kenney_player.png','assets/images/kenney_player_atlas.json');
-        this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
-
-        this.load.tilemapTiledJSON('map','assets/tilemaps/level1.json')
-
-
-
+        // chargement de la map en json
+        this.load.tilemapTiledJSON("map", "assets/MapBasique.json");
     }
 
 
     create() {
-        let me = this;
+
+        // Création du personnage de base
+        this.perso = this.physics.add.sprite(500, 0, 'circle').setOrigin(0, 0);
+        this.perso.setDisplaySize(30,30);
+        this.perso.body.setAllowGravity(true);
+        this.perso.setVisible(true);
+
+        // Création du personnage armé
+        this.persoA = this.physics.add.sprite(500, 0, 'circleG').setOrigin(0, 0);
+        this.persoA.setDisplaySize(30,30);
+        this.persoA.body.setAllowGravity(true);
+        this.persoA.setVisible(false);
+
+        // Création de l'arme qui sera au sol
+        this.arme = this.physics.add.sprite(150, 0,'platform').setOrigin(0, 0);
+        this.arme.setDisplaySize(15,15);
+        this.arme.body.setAllowGravity(true);
+        this.arme.setImmovable(true);
 
 
-        this.timerLife = 0
 
+        // chargement de la map
+        const map = this.add.tilemap("map");
+        // chargement du tileset
+        const tileset = map.addTilesetImage(
+            "game_tile",
+            "tilemap"
+        );
 
-
-
-
-        const backgroundImage = this.add.image(0, 0,'background').setOrigin(0, 0);
-        backgroundImage.setScale(2, 0.8);
-
-        const map = this.make.tilemap({ key: 'map' });
-
-        const tileset = map.addTilesetImage('kenny_simple_platformer', 'tiles');
-
-        const platforms = map.createLayer('Platforms', tileset, 0, 200);
+        // chargement du calque plateformes
+        const platforms = map.createLayer(
+            "calque_plateformes",
+            tileset
+        );
 
         platforms.setCollisionByExclusion(-1, true);
 
-        this.player = this.physics.add.sprite(500, 0, 'player');
-        this.player.setBounce(0.1);
-        this.player.setCollideWorldBounds(true);
-        this.physics.add.collider(this.player, platforms);
+        // Creation des collision
 
-        this.anims.create({
-            key: 'walk',
-            frames: this.anims.generateFrameNames('player', {
-                prefix: 'robo_player_',
-                start: 2,
-                end: 3,
-            }),
-            frameRate: 10,
-            repeat: -1
-        });
+        this.physics.add.collider(this.perso, platforms);
+        this.physics.add.collider(this.persoA, platforms);
+        this.physics.add.collider(this.arme, platforms);
 
-        this.anims.create({
-            key: 'idle',
-            frames: [{ key: 'player', frame: 'robo_player_0' }],
-            frameRate: 10,
-        });
-
-        this.anims.create({
-            key: 'jump',
-            frames: [{ key: 'player', frame: 'robo_player_1' }],
-            frameRate: 10,
-        });
-
-
-
-        this.perso = this.physics.add.sprite(500, 0, 'w').setOrigin(0, 0);
-        this.anims.create({
-            key: 'walk',
-            frames: this.getFrames("w", 4),
-            frameRate: 16,
-            repeat: -1,
-        });
-        this.perso.play('walk');
-        this.perso.setDisplaySize(20,30);
-        this.perso.body.setAllowGravity(true);
-        this.perso.setVisible(false);
-
-        //Mur bas
-        // this.bas = this.physics.add.sprite(0, 600-19,'platform').setOrigin(0, 0);
-        // this.bas.setDisplaySize(1000,20);
-        // this.bas.body.setAllowGravity(false);
-        // this.bas.setImmovable(true);
-
-
-        this.persoObject = this.physics.add.sprite(500, 0,'circle').setOrigin(0, 0);
-        this.persoObject.setDisplaySize(20,20);
-        this.persoObject.setVisible(true);
-
-        this.pile = this.physics.add.sprite(600, 0,'circleG').setOrigin(0, 0);
-        this.pile.setDisplaySize(20,20);
-        this.pile.setVisible(false);
-
-        //    Collision
-        // this.physics.add.collider(this.perso,this.bas);
-        // this.physics.add.collider(this.persoObject,this.bas);
-        // this.physics.add.collider(this.pile,this.bas);
-
-        // this.physics.add.overlap(this.perso, this.pile);
-
-        this.cursors = this.input.keyboard.createCursorKeys(); // viens de Tiled
         this.initKeyboard();
     }
 
-    getFrames(prefix, length) {
-        let frames = [];
-        for (let i = 1; i <= length; i++) {
-            frames.push({key: prefix + i});
-        }
-        return frames;
+    // fonction pour faire regarder s'il y a un overlaps donc deux objets qui se touche pour l'utilisé plus facilement.
 
-    }
-
-    checkCollider(Objet1x,Objet1y,Object1Tx,Object1Ty,Objet2x,Objet2y,Objet2Tx,Objet2Ty){
-        if (Objet1x + Object1Tx > Objet2x && Objet1x < Objet2x + Objet2Tx && Objet1y + Object1Ty > Objet2y && Objet1y < Objet2y + Objet2Ty) {
+    checkCollider(Objet1x,Objet1y,Object1TailleLargeur,Object1TailleHauteur,Objet2x,Objet2y,Objet2TaileLargeur,Objet2TailleHauteur){
+        if (Objet1x + Object1TailleLargeur > Objet2x && Objet1x < Objet2x + Objet2TaileLargeur
+                                            &&
+            Objet1y + Object1TailleHauteur > Objet2y && Objet1y < Objet2y + Objet2TailleHauteur) {
+            // Si toutes les conditons sont vrais alors il y a bien un overlaps, on renvoie donc true/vrai a notre foncion sinon on ne renvoie rien
             return true
         }
     }
@@ -135,126 +79,51 @@ class Tableau1 extends Phaser.Scene {
 
         this.input.keyboard.on('keyup', function (kevent) {
             switch (kevent.keyCode) {
-                case Phaser.Input.Keyboard.KeyCodes.Z:
 
-
-                    break;
                 case Phaser.Input.Keyboard.KeyCodes.Q:
-                    me.persoObject.setVelocityX(0)
+
                     me.perso.setVelocityX(0)
                     break;
-                case Phaser.Input.Keyboard.KeyCodes.S:
 
-                    break;
                 case Phaser.Input.Keyboard.KeyCodes.D:
-                    me.persoObject.setVelocityX(0)
+
                     me.perso.setVelocityX(0)
                     break;
             }
         })
         this.input.keyboard.on('keydown', function (kevent) {
             switch (kevent.keyCode) {
-                case Phaser.Input.Keyboard.KeyCodes.Z:
 
-
-                    break;
                 case Phaser.Input.Keyboard.KeyCodes.Q:
-                    if(me.perso.visible === false) {
-                        me.persoObject.setVelocityX(-200)
-                    }else {
+
+
                         me.perso.setVelocityX(-300)
-                    }
-                    break;
-                case Phaser.Input.Keyboard.KeyCodes.S:
-
-
 
                     break;
+
                 case Phaser.Input.Keyboard.KeyCodes.D:
-                    if(me.perso.visible === false) {
-                        me.persoObject.setVelocityX(200)
-                    }else {
+
                         me.perso.setVelocityX(300)
-                    }
+
                     break;
 
                 case Phaser.Input.Keyboard.KeyCodes.E:
-                    if(me.perso.visible === false) {
-                        me.pile.setVisible(true)
-                        // me.pile.disableBody(true)
-                        me.pile.x = me.persoObject.x
-                        me.pile.y = me.persoObject.y
-                        me.perso.x = me.persoObject.x
-                        me.perso.y = me.persoObject.y -20
-                        me.persoObject.setVisible(false)
-                        me.perso.setVisible(true)
 
-                    }else {
-                        if (me.checkCollider(me.perso.x,me.perso.y,20,30,me.pile.x,me.pile.y,20,20) === true) {
-                            me.persoObject.x = me.pile.x
-                            me.persoObject.y = me.pile.y
-
-                            me.persoObject.setVisible(true)
-                            me.perso.setVisible(false)
-                            me.pile.setVisible(false)
-                            // me.pile.disableBody(false)
-                            this.timerLife = 0
+                        if (me.checkCollider(me.perso.x,me.perso.y,20,30,me.arme.x,me.arme.y,15,15)
+                            ===
+                            true) {
+                           me.arme.setVisible(false);
+                           me.perso.setVisible(false);
+                            me.persoA.x = me.perso.x
+                            me.persoA.y = me.perso.y
+                           me.persoA.setVisible(true)
 
                         }
-                    }
-
                     break;
             }
         })
     }
 
-    update() {
 
-        if (this.cursors.left.isDown) {
-            this.player.setVelocityX(-200);
-            if (this.player.body.onFloor()) {
-                this.player.play('walk', true);
-            }
-        } else if (this.cursors.right.isDown) {
-            this.player.setVelocityX(200);
-            if (this.player.body.onFloor()) {
-                this.player.play('walk', true);
-            }
-        } else {
-
-            this.player.setVelocityX(0);
-
-            if (this.player.body.onFloor()) {
-                this.player.play('idle', true);
-            }
-        }
-
-
-        if ((this.cursors.space.isDown || this.cursors.up.isDown) && this.player.body.onFloor()) {
-            this.player.setVelocityY(-350);
-            this.player.play('jump', true);
-        }
-        if (this.player.body.velocity.x > 0) {
-            this.player.setFlipX(false);
-        } else if (this.player.body.velocity.x < 0) {
-            // otherwise, make them face the other side
-            this.player.setFlipX(true);
-        }
-
-
-
-
-
-
-        if(this.timerLife === 30){
-            console.log("mort mdr")
-            this.timerLife = 0
-        }
-
-        if (this.perso.visible === true) {
-            this.timerLife += 1
-        }else{
-            this.timerLife = 0
-        }
-    }
+    // fin du programme
 }
