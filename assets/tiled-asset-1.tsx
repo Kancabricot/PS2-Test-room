0af4e01aa5d7ac20563cf8d9d63a84ff2a3a1d03
Kancabricot <92451452+Kancabricot@users.noreply.github.com>
2022-04-13 22:16:01 +0200

    ajout de tiled

---
+++ assets/tiled-asset-1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tiled-asset-1" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="81" columns="9">
+ <image source="E:/PS2/tiled-asset-1.png" width="320" height="320"/>
+</tileset>
---
+++ assets/tiles-set-test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="tiles-set-test" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="E:/github/PS2-Test-room/assets/tiles-set-test.png" width="320" height="320"/>
+</tileset>
